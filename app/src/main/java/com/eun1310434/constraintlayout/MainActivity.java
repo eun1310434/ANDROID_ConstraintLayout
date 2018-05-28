256c4dd4a2c6988b4d660cd894ab91f613030916
eun1310434 <eun1310434@naver.com>
2018-05-28 10:40:17 +0200

    -

--- app/src/main/java/com/eun1310434/constraintlayout/MainActivity.java
+++ app/src/main/java/com/eun1310434/constraintlayout/MainActivity.java
@@ -1,8 +1,19 @@
-/**
- * 19.02.2018
- * eun1310434@naver.com
- * https://blog.naver.com/eun1310434
- */
+/*=====================================================================
+□ INFORMATION
+  ○ Data : 28.05.2018
+  ○ Mail : eun1310434@naver.com
+  ○ Blog : https://blog.naver.com/eun1310434
+  ○ Reference : Do it android app Programming
+
+□ FUNCTION
+  ○ XML을 활용한 Linear Layout Setting - BaseLine
+
+□ STUDY
+  ○ Constrain Layout
+     - ConstraintLayout allows you to create large and complex layouts with a flat view hierarchy (no nested view groups).
+       It's similar to RelativeLayout in that all views are laid out according to relationships between sibling views and the parent layout,
+       but it's more flexible than RelativeLayout and easier to use with Android Studio's Layout Editor.
+=====================================================================*/
 
 package com.eun1310434.constraintlayout;
 
